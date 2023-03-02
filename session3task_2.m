7522e957fdd9a973841d9cfcdbb84fd28b15ef9e
Ahmed Shaker <74692808+Shaker2001@users.noreply.github.com>
2023-03-02 12:10:29 +0200

    Add files via upload

---
+++ session3task_2.m
@@ -0,0 +1,18 @@
+clear,clc
+a =input('inter the number')
+
+switch a
+    case 10
+        disp('A')
+    case 9 
+        disp('A')
+    case 8 
+         disp('B')
+    case 7
+         disp('C')
+    case 6
+         disp('D')
+    otherwise
+        disp('F')
+
+end
